3ab5962ff5405f01ac4935423160bdc801caf6e5
DedlySpyder <pooreboy464@gmail.com>
2016-06-26 12:44:26 -0400

    0.3.2 Changes
    -Compatibility changes for Factorio 0.13 (hopefully)

--- Avatars_0.3.2/control.lua
+++ Avatars_0.3.2/control.lua
@@ -1,4 +1,3 @@
-require "defines"
 require "gui"
 require "config"
 require "scripts"
@@ -15,7 +14,7 @@ end)
 
 --Check on entering or leaving a vehicle
 function on_driving(event)
-	local player = game.get_player(event.player_index)
+	local player = game.players[event.player_index]
 	
 	--Check for entering the Avatar Control Center
 	if player.vehicle and player.vehicle.name == "avatar-control-center" then
@@ -41,7 +40,7 @@ script.on_event(defines.events.on_player_driving_changed_state, on_driving)
 function checkGUI(event)
 	local element = event.element
 	local elementName = element.name
-	local player = game.get_player(event.player_index)
+	local player = game.players[event.player_index]
 	debugLog("Clicked "..elementName)
 	
 	--Page forward button
