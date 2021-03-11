0ee845027007e14af558ae1f90a5224761d32dbc
DedlySpyder <dedlyspyder@gmail.com>
2021-03-10 20:55:34 -0500

    Reworked enterable entity prototypes to be 2 separate entities (real entity with hidden car)

--- prototypes/entities/enterable-entities.lua
+++ prototypes/entities/enterable-entities.lua
@@ -1,155 +1,131 @@
-data:extend({
+require ("circuit-connector-sprites")
+
+local base_internal_car = {
+	type = "car",
+	icon_size = 32,
+	
+	flags = {"placeable-neutral", "player-creation", "not-rotatable"},
+	flags = {"not-on-map", "not-blueprintable", "not-deconstructable", "hidden", "hide-alt-info", "not-flammable", "no-automated-item-removal", "no-automated-item-insertion", "no-copy-paste", "not-selectable-in-game", "not-upgradable", "not-in-kill-statistics"},
+	max_health = 2147483648,
+	collision_mask = {},
+	
+	-- Vehicle
+	weight = 0.1,
+	braking_power = "1W",
+	friction = 0.1,
+	energy_per_hit_point = 1,
+	
+	-- Car
+	animation = {
+		filename = "__Avatars__/graphics/entity/avatar-control-center.png", --TODO give this a null image
+		direction_count = 1,
+		size = 1
+	},
+	effectivity = 0,
+	consumption = "0W",
+	rotation_speed = 0,
+	energy_source = {
+		type = "void"
+	},
+	inventory_size = 0,
+}
+
+function make_internal_car(base_entity_name)
+	local new_entity = util.table.deepcopy(base_internal_car)
+	new_entity["name"] = base_entity_name .. "-internal"
+	return new_entity
+end
+
+local vehicle_impact_sound = {
 	{
+		filename = "__base__/sound/car-metal-impact-2.ogg", volume = 0.5
 	},
 	{
-		type = "car",
+		filename = "__base__/sound/car-metal-impact-3.ogg", volume = 0.5
+	},
+	{
+		filename = "__base__/sound/car-metal-impact-4.ogg", volume = 0.5
+	},
+	{
+		filename = "__base__/sound/car-metal-impact-5.ogg", volume = 0.5
+	},
+	{
+		filename = "__base__/sound/car-metal-impact-6.ogg", volume = 0.5
+	}
+}
+
+data:extend({
+	{
+		type = "simple-entity-with-force",
 		name = "avatar-control-center",
 		icon = "__Avatars__/graphics/icons/avatar-control-center.png",
 		icon_size = 32,
-		flags = {"placeable-neutral", "player-creation"},
-		minable = {mining_time = 3, result = "avatar-control-center"},
+		flags = {"not-rotatable", "placeable-player", "player-creation", "not-upgradable"},
+		minable = {mining_time = 1, result = "avatar-control-center"},
 		mined_sound = { filename = "__core__/sound/deconstruct-large.ogg" },
 		max_health = 200,
 		corpse = "big-remnants",
 		dying_explosion = "massive-explosion",
-		energy_per_hit_point = 1,
 		resistances = {
 			{
 				type = "fire",
-				percent = 50
+				percent = 70
+			},
+			{
+				type = "impact",
+				percent = 30
 			}
 		},
 		collision_box = {{-1.4, -1.2}, {1.5, 1.2}},
 		selection_box = {{-1.65, -1.65}, {1.65, 1.75}},
-		effectivity = 0,
-		braking_power = "1W",
-		energy_source = {
-			type = "void"
-		},
-		consumption = "0W",
-		friction = 1,
-		light = {{minimum_darkness = 0.3,intensity = 0, size = 0}},
-		pictures = {
-			filename = "__Avatars__/graphics/entity/avatar-control-center.png",
-			priority = "low",
+		animations = {
 			width = 169,
 			height = 140,
-			apply_projection = false,
-			direction_count = 64,
+			frame_count = 64,
 			line_length = 8,
+			shift = {1.2, 0.8},
+			animation_speed = 0.5,
+			scale = 1,
+			filename = "__Avatars__/graphics/entity/avatar-control-center.png",
 		},
-		animation = {
-			layers = {
-				{
-					width = 169,
-					height = 140,
-					frame_count = 8,
-					direction_count = 1,
-					shift = {1.2, 0.8},
-					animation_speed = 5,
-					max_advance = 1,
-					scale = 1,
-					priority = "extra-high",
-					filename = "__Avatars__/graphics/entity/avatar-control-center.png",
-				},
-			}
-		},
-		sound_minimum_speed = 0.2;
-		vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.75 },
-		working_sound = {
-			sound =
-			{
-				filename = "__base__/sound/radar.ogg",
-				volume = 0.6
-			},
-			activate_sound =
-			{
-				filename = "__base__/sound/radar.ogg",
-				volume = 0
-			},
-			deactivate_sound =
-			{
-				filename = "__base__/sound/radar.ogg",
-				volume = 0
-			},
-			match_speed_to_activity = false,
-		},
-		rotation_speed = 0,
-		weight = 700,
-		inventory_size = 0
+		vehicle_impact_sound = vehicle_impact_sound
 	},
 	{
-		type = "car",
+		type = "container",
 		name = "avatar-remote-deployment-unit",
 		icon = "__Avatars__/graphics/icons/avatar-remote-deployment-unit.png",
 		icon_size = 32,
-		flags = {"placeable-neutral", "player-creation", "not-rotatable"},
-		minable = {mining_time = 3, result = "avatar-remote-deployment-unit"},
+		flags = {"not-rotatable", "placeable-player", "player-creation", "not-upgradable"},
+		minable = {mining_time = 1, result = "avatar-remote-deployment-unit"},
 		mined_sound = { filename = "__core__/sound/deconstruct-large.ogg" },
 		max_health = 200,
 		corpse = "big-remnants",
 		dying_explosion = "massive-explosion",
-		energy_per_hit_point = 1,
 		resistances = {
 			{
 				type = "fire",
-				percent = 50
+				percent = 70
+			},
+			{
+				type = "impact",
+				percent = 30
 			}
 		},
 		collision_box = {{-1.4, -1.2}, {0.6, 1.2}},
 		selection_box = {{-1.65, -1.65}, {0.75, 1.75}},
-		effectivity = 0,
-		braking_power = "1W",
-		energy_source = {
-			type = "void"
-		},
-		consumption = "0W",
-		friction = 1,
-		light = {{minimum_darkness = 0.3,intensity = 0, size = 0}},
-		pictures = {
+		picture = {
 			filename = "__Avatars__/graphics/entity/avatar-remote-deployment-unit.png",
-			priority = "low",
 			width = 160,
 			height = 128,
-			apply_projection = false,
-			direction_count = 64,
-			line_length = 8,
+			shift = {0.3, 0},
+			scale = 0.75
 		},
-		animation = {
-			layers = {
-				{
-					width = 160,
-					height = 128,
-					frame_count = 1,
-					direction_count = 1,
-					shift = {0.3, 0},
-					animation_speed = 5,
-					max_advance = 1,
-					scale = 0.75,
-					priority = "extra-high",
-					filename = "__Avatars__/graphics/entity/avatar-remote-deployment-unit.png",
-				},
-			}
-		},
-		sound_minimum_speed = 0.2;
-		vehicle_impact_sound =  { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.75 },
-		working_sound = {
-			sound = {
-				filename = "__base__/sound/radar.ogg",
-				volume = 0.6
-			},
-			activate_sound = {
-				filename = "__base__/sound/radar.ogg",
-				volume = 0
-			},
-			deactivate_sound = {
-				filename = "__base__/sound/radar.ogg",
-				volume = 0
-			},
-			match_speed_to_activity = false,
-		},
-		rotation_speed = 0,
-		weight = 700,
-		inventory_size = 3
-	}
+		vehicle_impact_sound = vehicle_impact_sound,
+		inventory_size = 3,
+		circuit_wire_connection_point = circuit_connector_definitions["chest"].points,
+		circuit_connector_sprites = circuit_connector_definitions["chest"].sprites,
+		circuit_wire_max_distance = default_circuit_wire_max_distance
+	},
+	make_internal_car("avatar-control-center"),
+	make_internal_car("avatar-remote-deployment-unit")
 })
