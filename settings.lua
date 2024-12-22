c353b0568407598a53e79134cf810dda5c2a14f8
DedlySpyder <dedlyspyder@gmail.com>
2024-12-22 13:48:57 -0500

    Double settings fixes

--- settings.lua
+++ settings.lua
@@ -50,32 +50,32 @@ data:extend({
 		name = "Avatars_avatar_color_red",
 		type = "double-setting",
 		setting_type = "startup",
-		default_value = "0.737",
-		minimum_value = "0",
-		maximum_value = "1"
+		default_value = 0.737,
+		minimum_value = 0,
+		maximum_value = 1
 	},
 	{
 		name = "Avatars_avatar_color_green",
 		type = "double-setting",
 		setting_type = "startup",
-		default_value = "0.776",
-		minimum_value = "0",
-		maximum_value = "1"
+		default_value = 0.776,
+		minimum_value = 0,
+		maximum_value = 1
 	},
 	{
 		name = "Avatars_avatar_color_blue",
 		type = "double-setting",
 		setting_type = "startup",
-		default_value = "0.90",
-		minimum_value = "0",
-		maximum_value = "1"
+		default_value = 0.90,
+		minimum_value = 0,
+		maximum_value = 1
 	},
 	{
 		name = "Avatars_avatar_color_alpha",
 		type = "double-setting",
 		setting_type = "startup",
-		default_value = "0.7",
-		minimum_value = "0",
-		maximum_value = "1"
+		default_value = 0.7,
+		minimum_value = 0,
+		maximum_value = 1
 	}
 })
