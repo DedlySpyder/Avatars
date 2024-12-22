d0e2e7b3714cd8ec180f71e441e7ba1aa4758292
DedlySpyder <dedlyspyder@gmail.com>
2024-12-22 15:35:56 -0500

    Added space age integration

--- prototypes/mod-integration/util.lua
+++ prototypes/mod-integration/util.lua
@@ -16,6 +16,15 @@ function Util.Tech.removePrerequisite(techName, prereq)
 	data.raw["technology"]["avatars"].prerequisites = newPreqs
 end
 
+function Util.Tech.addIngredient(techName, ingredient)
+	local techIngredients = data.raw["technology"][techName].unit.ingredients
+	if type(ingredient) == "string" then
+		table.insert(techIngredients, {ingredient, 1})
+	else
+		table.insert(techIngredients, ingredient)
+	end
+end
+
 Util.Recipe = {}
 
 function Util.Recipe.replaceIngredientName(recipeName, oldIngredient, newIngredient)
@@ -26,4 +35,18 @@ function Util.Recipe.replaceIngredientName(recipeName, oldIngredient, newIngredi
 			ingredient["name"] = newIngredient
 		end
 	end
+end
+
+function Util.Recipe.replaceIngredient(recipeName, oldIngredientName, newIngredient)
+	local recipeIngredients = data.raw["recipe"][recipeName].ingredients
+
+	for i, ingredient in ipairs(recipeIngredients) do
+		if ingredient["name"] and ingredient["name"] == oldIngredientName then
+			recipeIngredients[i] = newIngredient
+		end
+	end
+end
+
+function Util.Recipe.setCategory(recipeName, category)
+	data.raw["recipe"][recipeName].category = category
 end
