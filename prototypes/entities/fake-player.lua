01fdd209188030cb51bcea3a3d5e6701639b94ad
DedlySpyder <dedlyspyder@gmail.com>
2024-12-22 15:36:12 -0500

    Hid the fake player from UIs

--- prototypes/entities/fake-player.lua
+++ prototypes/entities/fake-player.lua
@@ -2,5 +2,6 @@ local fakePlayer = util.table.deepcopy(data.raw["character"]["character"])
 
 fakePlayer.name = "fake-player"
 fakePlayer.character_corpse = nil
+fakePlayer.hidden = true
 
 data:extend ({ fakePlayer })
