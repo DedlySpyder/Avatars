05e1143e20f1abf4c8632bde9f62aa8e7941e6c4
Verchi März Frye <verchi.mart.frye@gmail.com>
2026-01-12 23:02:56 +0100

    updated mod-integration/space-exploration.lua; reenabled SE mod integration in data.lua

--- prototypes/mod-integration/space-exploration.lua
+++ prototypes/mod-integration/space-exploration.lua
@@ -1,7 +1,8 @@
 -- Space Exploration
 if mods["space-exploration"] then
+	Util.Tech.removePrerequisite("avatars", "fission-reactor-equipment")
 	Util.Tech.addPrerequisite("avatars", "se-rtg-equipment")
 	
-	Util.Recipe.replaceIngredientName("avatar-control-center", "fusion-reactor-equipment", "se-rtg-equipment")
-	Util.Recipe.replaceIngredientName("avatar-internals", "fusion-reactor-equipment", "se-rtg-equipment")
+	Util.Recipe.replaceIngredientName("avatar-control-center", "fission-reactor-equipment", "se-rtg-equipment")
+	Util.Recipe.replaceIngredientName("avatar-internals", "fission-reactor-equipment", "se-rtg-equipment")
 end
